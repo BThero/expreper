b650af370ad6eaf973eca4bc4f9143e421fdda35
Temirlan <bthero09@gmail.com>
2025-12-23 01:51:42 +0800

    add a single signed 128-bit integer support

--- main.zig
+++ main.zig
@@ -1,12 +1,32 @@
 const std = @import("std");
 
 const usage =
-    \\Usage: ./expreper [options]
+    \\Usage: ./expreper [options] [expr]
     \\
     \\Options:
     \\ -h, --help: Show this usage information
+    \\Expr:
+    \\ A single integer which fits in a 128-bit signed type.
     \\
 ;
+
+fn parse_i128(line: []u8) !i128 {
+    const num = try std.fmt.parseInt(i128, line, 10);
+    return num;
+}
+
+fn stdout_print(comptime fmt: []const u8, args: anytype) !void {
+    const stdout = std.fs.File.stdout();
+    var buffer: [64]u8 = undefined;
+    var stdout_file_writer: std.fs.File.Writer = .{
+        .interface = std.fs.File.Writer.initInterface(&buffer),
+        .file = stdout,
+        .mode = .streaming,
+    };
+    const stdout_writer = &stdout_file_writer.interface;
+    try stdout_writer.print(fmt, args);
+    try stdout_writer.flush();
+}
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -17,14 +37,19 @@ pub fn main() !void {
         while (i < args.len) : (i += 1) {
             const arg = args[i];
             if (std.mem.eql(u8, "-h", arg) or std.mem.eql(u8, "--help", arg)) {
-                try std.fs.File.stdout().writeAll(usage);
+                try stdout_print("{s}", .{usage});
                 return std.process.cleanExit();
             } else {
-                std.debug.print("Unrecognized command-line argument: '{s}'", .{arg});
-                std.process.exit(1);
+                const num = parse_i128(arg) catch {
+                    std.debug.print("Could not parse integer: '{s}'\n", .{arg});
+                    std.process.exit(1);
+                };
+                try stdout_print("Successfully parsed: {}\n", .{num});
+                return std.process.cleanExit();
             }
         }
     }
 
-    std.debug.print("Hello from expreper!\n", .{});
+    std.debug.print("{s}", .{usage});
+    std.process.exit(1);
 }
