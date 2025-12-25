5aff39980e459452246e3b26119887c3293157ad
Temirlan <bthero09@gmail.com>
2025-12-26 02:11:53 +0800

    add plus operator support

--- main.zig
+++ main.zig
@@ -15,6 +15,17 @@ fn parse_i128(line: []u8) !i128 {
     return num;
 }
 
+fn parse_expr(line: []u8) !i128 {
+    const maybe_plus_pos = std.mem.indexOf(u8, line, "+");
+    if (maybe_plus_pos == null) {
+        return parse_i128(line);
+    }
+    const plus_pos = maybe_plus_pos.?;
+    const lhs = try parse_expr(line[0..plus_pos]);
+    const rhs = try parse_expr(line[plus_pos + 1 .. line.len]);
+    return try std.math.add(i128, lhs, rhs);
+}
+
 fn stdout_print(comptime fmt: []const u8, args: anytype) !void {
     const stdout = std.fs.File.stdout();
     var buffer: [64]u8 = undefined;
@@ -40,8 +51,8 @@ pub fn main() !void {
                 try stdout_print("{s}", .{usage});
                 return std.process.cleanExit();
             } else {
-                const num = parse_i128(arg) catch {
-                    std.debug.print("Could not parse integer: '{s}'\n", .{arg});
+                const num = parse_expr(arg) catch {
+                    std.debug.print("Could not parse expression: '{s}'\n", .{arg});
                     std.process.exit(1);
                 };
                 try stdout_print("Successfully parsed: {}\n", .{num});
