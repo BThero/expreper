36abe470d1b474009c6976d8d0fe7bdf08b6a051
Temirlan <bthero09@gmail.com>
2026-01-07 01:35:38 +0800

    add evaluator tests

--- main.test.zig
+++ main.test.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const tokenizer_tests = @import("tokenizer.test.zig");
+const evaluator_tests = @import("evaluator.test.zig");
 
 test {
     _ = tokenizer_tests;
+    _ = evaluator_tests;
 }
